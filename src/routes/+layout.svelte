5c8979f650a4c897c7b09ee77bcc88725ccb2e5b
Wytse Monsma <wytse.monsma@gmail.com>
2023-07-11 22:59:50 +0200

    Beunhandel fadeIN fadeOUT

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,17 @@
-<script lang='ts'>
-    import { isMobile } from "./stores";
+<script lang="ts">
+    import { isMobile, toggled } from "./stores";
     import { useMediaQuery } from "svelte-breakpoints";
     import Navbar from "$lib/components/Navbar.svelte";
     import Background from "$lib/components/Background.svelte";
     import "../app.css";
-    
-    const mobile = useMediaQuery('(max-width: 768px)');
-    $: $mobile, $isMobile = $mobile;
 
+    const mobile = useMediaQuery("(max-width: 768px)");
+    $: $mobile, ($isMobile = $mobile);
 </script>
-  
-<div id='body-div' class='grid-rows-[6rem_1fr] grid gap-0 h-[100dvh]'>
+
+<div id="body-div" class="grid-rows-[6rem_1fr] grid gap-0 h-[100dvh]">
     <Navbar />
-    <main class='flex flex-col justify-center w-full m-0 text-light1'>
+    <main class="flex flex-col justify-center w-full m-0 text-light1" id="{$toggled ? "fadeOUT" : "fadeIN"}">
         <slot />
     </main>
     <Background />
