ae6ea8b373bcab6b6fa8e50a827b9d204d989bd3
Stijn Nijhuis <Spg.nijhuis@hotmail.com>
2023-07-13 16:02:16 +0200

    Remove svelte-breakpoints

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
     import { isMobile, toggled } from "./stores";
-    import { useMediaQuery } from "svelte-breakpoints";
     import Navbar from "$lib/components/Navbar.svelte";
     // import Background from "$lib/components/Background.svelte";
     import Flower from "$lib/components/Flower.svelte";
     import "../app.css";
 
-    const mobile = useMediaQuery("(max-width: 768px)");
-    $: $mobile, ($isMobile = $mobile);
+    let w: number;
+    $: {w < 768 ? $isMobile = true : $isMobile = false;}
 </script>
 
 <div id="body-div" class="grid-rows-[6rem_1fr] grid gap-0 h-[100dvh]">
-
     <Navbar />
     <main class="flex flex-col justify-center w-full m-0 text-light1" id="{$toggled ? "fadeOUT" : "fadeIN"}">
         <slot />
@@ -19,3 +17,4 @@
     <!-- <Background /> -->
     <Flower />
 </div>
+<svelte:window bind:innerWidth={w} />
