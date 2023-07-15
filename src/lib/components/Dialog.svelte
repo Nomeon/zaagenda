223d9d912a2a6c36818204cb679f08f87c34a89a
Stijn Nijhuis <Spg.nijhuis@hotmail.com>
2023-07-15 23:40:04 +0200

    Update Dialog.svelte

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -1,13 +1,7 @@
 <script lang='ts'>
 	export let dialog: HTMLDialogElement;
-
-    function onKeydown(event: KeyboardEvent) {
-        if (event.key === 'Escape') {
-            dialog.close();
-        }
-    }
 </script>
 
-<dialog on:keydown={(e) => onKeydown(e)} bind:this={dialog} on:click={() => dialog.close()} id='dialog-modal' class='relative p-0 rounded-lg bg-dark1 text-light1 overflow-hidden'>
+<dialog bind:this={dialog} on:click={() => dialog.close()} id='dialog-modal' class='relative p-0 rounded-lg bg-dark1 text-light1 overflow-hidden'>
 	<slot/>
 </dialog>
