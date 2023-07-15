70c5f05d2ce25c9d016c5e12d56727665555731b
Stijn Nijhuis <Spg.nijhuis@hotmail.com>
2023-07-15 23:41:56 +0200

    Update Dialog.svelte

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -2,6 +2,6 @@
 	export let dialog: HTMLDialogElement;
 </script>
 
-<dialog bind:this={dialog} on:click={() => dialog.close()} id='dialog-modal' class='relative p-0 rounded-lg bg-dark1 text-light1 overflow-hidden'>
+<dialog bind:this={dialog} id='dialog-modal' class='relative p-0 rounded-lg bg-dark1 text-light1 overflow-hidden'>
 	<slot/>
 </dialog>
